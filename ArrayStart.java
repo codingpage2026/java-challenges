c3416320bf128ae5c75d2a8f2296790324d8d461
YASH MISHRA <codingpage2026@gmail.com>
2026-09-13 10:35:36 +0530

    Add files via upload

---
+++ ArrayStart.java
@@ -0,0 +1,31 @@
+public class ArrayStart {
+    public static void main(String[] args) {
+    //     int[] myarray = {2,45,74,9,0,32,74};
+    //         int index = 0;
+    //         while (index<myarray.length) {
+    //         System.out.println(myarray[index]);
+    //         index++;
+    //      }
+            
+    //      Scanner input =new Scanner(System.in);
+    //     int[] arr = {88 , 90, 93,33,44,89,822 ,82};
+    //     System.out.print("\n ****** wellcome to array search **** \n\n enter the number you want to search :");
+    //     int a =input.nextInt();
+    //     isfound(arr, a);
+        
+    // }
+    // public static int isfound(int[] arr ,int a){
+    //     int index = 0;
+    //     while (index<arr.length) { 
+    //         if (arr[index]==a) { 
+    //             System.out.println(" your no. was found in the array");
+    //             return 0;
+    //         }
+    //         index++;
+    //     }
+    //       System.out.println(" your no. is not found in the array");
+    //       return 0;
+        
+       
+    }
+}
